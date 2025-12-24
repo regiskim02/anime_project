0d7cc7cbea392b013d1a3f5ce79ddcbf8677318c
regiskim02 <regiskim987@gmail.com>
2025-12-24 20:54:39 +0900

    starting

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+
+`
+
+
+
+function Header() {
+    return(
+        <StyledHeader>
+
+        </StyledHeader>
+    );
+}
+
+export default Header;
